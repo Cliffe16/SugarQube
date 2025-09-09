b6bf85ebb80148ae712cefb94adaf0d0eef1ae63
Cliffe16 <oluochcliffe13@gmail.com>
2025-09-09 15:36:32 +0300

    Change dashboard lauyout

--- users/views.py
+++ users/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import logout
-from django.contrib import messages as django_messages
+from .forms import CustomUserCreationForm  # Import the new form
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)  # Use the new form
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}! You can now log in.')
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()  # Use the new form
     return render(request, 'registration/register.html', {'form': form})
-
